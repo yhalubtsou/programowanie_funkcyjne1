7a2924839415e3c19fe79ac1e0dd6a706786bcc7
yhalubtsou <yarashalub@gmail.com>
2024-04-22 09:58:05 +0200

    pakiet 1-5

--- Pakiet5/zad2.py
+++ Pakiet5/zad2.py
@@ -4,9 +4,9 @@
 #    return a + b
 
 # Wczytanie funkcji z pliku math_operations.py
-#import math_operations
+import math_operations
 
 # Wywołanie wczytanej funkcji
-#result = math_operations.add_numbers(5, 3)
-#print("Wynik dodawania:", result)
+result = math_operations.add_numbers(5, 3)
+print("Wynik dodawania:", result)
 
